12f856824fd0fac4138fece9d772f1d2c8b6abf3
VenturaAdrian <venturaadrian999@gmail.com>
2026-07-07 13:48:59 +0800

    test push

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
     // baseApi: 'http://localhost:5000/api',
 
-    baseApi: 'https://wyfmw.netlify.app/api',
+    baseApi: 'https://wyfmw-production.up.railway.app/api',
 };
 
 export default config;
