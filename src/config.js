d51c53d738cd941970d4ec3dd114073c8ea99e3f
VenturaAdrian <venturaadrian999@gmail.com>
2026-07-07 13:28:30 +0800

    asdasd

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 const config = {
-    baseApi: 'http://localhost:5000/api',
+    // baseApi: 'http://localhost:5000/api',
+
+    baseApi: 'http://https://wyfmw.netlify.app/api',
 };
 
 export default config;
